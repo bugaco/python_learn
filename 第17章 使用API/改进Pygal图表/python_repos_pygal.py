4108041e93814d29473cb5d224badd9092363db6
Liyz <liyizhezhe@gmail.com>
2017-01-18 15:37:07 +0800

    test commit

--- 第17章 使用API/改进Pygal图表/python_repos_pygal.py
+++ 第17章 使用API/改进Pygal图表/python_repos_pygal.py
@@ -12,31 +12,27 @@ print("Status code:", r.status_code)
 response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
-# 探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
 names, stars = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
-    print("\nSelected infomation about first repository:")
-    print('Name:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 # chart = pygal.Bar(x_label_rotation=45, show_legend=False)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
@@ -44,7 +40,6 @@ chart.x_labels = names
 chart.add('', stars)
 chart.render_to_file('python_repos.svg')
 
-# 处理结果
-print(response_dict.keys())
+
 
 
